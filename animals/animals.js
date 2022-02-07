dc91694abeb0b3453e32ee63419609c2f92b92e5
Bradley-Bird <Bradleybird2@icloud.com>
2022-02-07 15:44:49 -0800

    added new search params to select from array id. made function to display focus animal on new ppage

--- animals/animals.js
+++ animals/animals.js
@@ -1,3 +1,14 @@
 import { animals } from '../data.js';
+import { renderFocusItem } from '../render-utils.js';
 import { findById } from '../utils.js';
-console.log('henlo')
+// console.log('henlo')
+const focusAnimal = document.getElementById('animal-info');
+
+const params = new URLSearchParams(window.location.search);
+// console.log(params.get('id'));
+const animal = findById(params.get('id'), animals);
+// console.log(animal);
+
+const renderFocus = renderFocusItem(animal);
+
+focusAnimal.append(renderFocus);
--- render-utils.js
+++ render-utils.js
@@ -7,6 +7,7 @@ export function renderListItem(animal) {
 
     const a = document.createElement('a');
     a.href = `/animals/?id=${animal.id}`;
+    a.setAttribute('target', '_blank');
 
     const img = document.createElement('img');
     img.src = `./assets/${animal.type}.svg`;
@@ -20,5 +21,23 @@ export function renderListItem(animal) {
 
     a.append(img);
     div.append(a, nameSpan, span);
+    return div;
+}
+
+export function renderFocusItem(animal) {
+    const div = document.createElement('div');
+    div.classList.add('animal');
+    const img = document.createElement('img');
+    img.src = `../assets/${animal.type}.svg`;
+    // img.style.height = '200px';
+
+    const nameSpan = document.createElement('span');
+    nameSpan.textContent = animal.name;
+    nameSpan.classList.add('name');
+
+    const span = document.createElement('span');
+    span.textContent = animal.says;
+
+    div.append(img, nameSpan, span);
     return div;
 }
